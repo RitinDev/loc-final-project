c7d2340f4cc37478b83e5be8fe1c4c1287da4000
Ritin Malhotra <rm5486@nyu.edu>
2023-04-17 22:04:11 +0400

    Add Functions to Display Table

--- list-handler.js
+++ list-handler.js
@@ -13,7 +13,7 @@ const generateUniqueID = () => {
     const id = Math.random().toString(36).slice(-5);
     // Go through the data and check if the ID already exists
     for (let i = 0; i < data.length; i++) {
-        if (data[i].id === id) {
+        if (data[i].list_code === id) {
             // If the ID exists, generate a new one
             return generateUniqueID();
         }
@@ -46,4 +46,80 @@ const newList = () => {
     return list;
 }
 
-// newList();
+// Function to get a list by its code and password
+const getListByCodeAndPassword = (code, password) => {
+    // Go through the data and find the list with the given code and password
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].list_code === code && data[i].list_password === password) {
+            // Return the list if found
+            return data[i];
+        }
+    }
+    // Return null if the list is not found
+    return null;
+}
+
+const bootstrapBadgeTypes = ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark'];
+
+// Function to get a random bootstrap badge type
+const getRandomBootstrapBadgeType = () => {
+    // Get a random index
+    const index = Math.floor(Math.random() * bootstrapBadgeTypes.length);
+    // Return the badge type at the random index
+    return bootstrapBadgeTypes[index];
+}
+
+const BeginDOMTable = () => {
+    
+}
+
+// Function to get a list and display its tasks in the DOM
+const displayList = (list) => {
+    // Create the table element and add the table classes
+    const table = document.createElement('table');
+    table.classList.add('table', 'table-striped', 'table-dark', 'table-hover');
+    const thead = document.createElement('thead');
+    const tr = document.createElement('tr');
+    const th1 = document.createElement('th');
+    th1.setAttribute('scope', 'col');
+    th1.textContent = 'Username';
+    const th2 = document.createElement('th');
+    th2.setAttribute('scope', 'col');
+    th2.textContent = 'Description';
+    const th3 = document.createElement('th');
+    th3.setAttribute('scope', 'col');
+    th3.textContent = 'Done';
+    tr.appendChild(th1);
+    tr.appendChild(th2);
+    tr.appendChild(th3);
+    thead.appendChild(tr);
+    table.appendChild(thead);
+
+    // Create the table body element
+    const tbody = document.createElement('tbody');
+    // Go through the list tasks and display them
+    for (let i = 0; i < list.list_tasks.length; i++) {
+        const task = list.list_tasks[i];
+        const tr = document.createElement('tr');
+        const td1 = document.createElement('td');
+        const span = document.createElement('span');
+        span.classList.add('badge', `bg-${getRandomBootstrapBadgeType()}`);
+        span.textContent = task.task_made_by;
+        td1.appendChild(span);
+        const td2 = document.createElement('td');
+        td2.textContent = task.task_description;
+        const td3 = document.createElement('td');
+        const button = document.createElement('button');
+        button.classList.add('btn', 'btn-sm', 'btn-outline-secondary');
+        button.setAttribute('type', 'button');
+        button.textContent = 'Mark as Done';
+        td3.appendChild(button);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tbody.appendChild(tr);
+    }
+    table.appendChild(tbody);
+    // Append the table to the main element
+    document.querySelector('main').appendChild(table);
+}

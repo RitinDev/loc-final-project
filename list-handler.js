54a1696f0ec8803576c1abcde2e3ea8a2e322653
Ritin Malhotra <rm5486@nyu.edu>
2023-04-19 21:19:20 +0400

    Add Some Backend Code for New Lists + Remove Boilerplate SVG

--- list-handler.js
+++ list-handler.js
@@ -1,48 +1,26 @@
-const fs = require('fs');
-
-// Read the lists data
-const jsonData = fs.readFileSync('./lists.json');
-// Parse the JSON data
-const data = JSON.parse(jsonData);
-// Print the data
-console.log(data);
-
 // Function to generate a unique ID for a new list
-const generateUniqueID = () => {
+const generateUniqueID = (lists) => {
     // Generate a string comprising 5 random characters
     const id = Math.random().toString(36).slice(-5);
     // Go through the data and check if the ID already exists
-    for (let i = 0; i < data.length; i++) {
-        if (data[i].list_code === id) {
+    lists.forEach((list) => {
+        if (list.list_code === id) {
             // If the ID exists, generate a new one
-            return generateUniqueID();
+            return generateUniqueID(lists);
         }
-    }
+    });
     // If the ID doesn't exist, return it
     return id;
 }
 
-// Function to create a password for a list
-// const generatePassword = () => {
-//     // Generate a string comprising 6 random characters
-//     const password = Math.random().toString(36).slice(-6);
-//     // Return the password
-//     return password;
-// }
-
 // Function to create a new list and add it to the data
-const newList = (password) => {
+const newList = (id, password) => {
     // Create  a new list object
     const list = {
-        list_code: generateUniqueID(),
+        list_code: id,
         list_password: password,
         list_tasks: []
     };
-    // Add the new list to the data
-    data.push(list);
-    // Write the new data to the JSON file
-    fs.writeFileSync('./lists.json', JSON.stringify(data));
-    // Return the new list
     return list;
 }
 
@@ -170,3 +148,13 @@ const addTask = (list, task) => {
     tr.appendChild(td3);
     tbody.appendChild(tr);
 }
+
+// Export the functions
+module.exports = {
+    generateUniqueID,
+    getListByCode,
+    getListByCodeAndPassword,
+    newList,
+    displayList,
+    addTask
+};
--- server.js
+++ server.js
@@ -3,23 +3,37 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 
 const app = express();
-const port = 3000;
+const port = 3000; // Port to run the server on
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.static(__dirname));
+const listHandler = require('./list-handler');
+
+app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use(bodyParser.json()); // Parse JSON bodies
+app.use(express.static(__dirname)); // Serve static files from the current directory
 
 app.get('/new-list.html', (req, res) => {
     res.sendFile(__dirname + '/new-list.html');
 });
 
 app.post('/new-list.html', (req, res) => {
-    const newList = req.body;
     fs.readFile('lists.json', (err, data) => {
         if (err) throw err;
 
+        // Get data from form in new-list.html
+        const name = req.body.name;
+        const password = req.body.password;
+        
+        // Get the data from lists.json
         const lists = JSON.parse(data);
-        lists.push(newList);
+
+        // Generate a unique ID for the new list
+        const id = listHandler.generateUniqueID(lists);
+
+        // Generate a new list object
+        const list = listHandler.newList(id, password);
+
+        // Add the new list to the data
+        lists.push(list);
 
         fs.writeFile('lists.json', JSON.stringify(lists), (err) => {
             if (err) throw err;
